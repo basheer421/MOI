88a75da4d1d7fbe724e2a4b898363c1d6c4b22bc
basheer421 <basheer753@gmail.com>
2024-05-11 22:44:40 +0400

    added voice support, but crashing without pop

--- src_unity/Assets/Scripts/UI_Manager.cs
+++ src_unity/Assets/Scripts/UI_Manager.cs
@@ -1,30 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class UI_Manager : MonoBehaviour
 {
+	// public Text uiText;
 	public List<user_info> info;
 	public int user;
-	public void	sign_in()
+
+	public Button EnableAssistantButton;
+	public Button DisableAssistantButton;
+	public VoiceManager Assistant;
+
+	public void sign_in()
 	{
 		int rand = Random.Range(0, 2);
 		user = rand;
 		Debug.Log("the user selected is " + info[user]);
 		//switch 
 	}
-    public void undeveloped_services()
+	public void undeveloped_services()
 	{
 		Debug.Log("Coming Soon!");
 	}
 
-	public void	vehicle_services()
+	public void vehicle_services()
 	{
 		//camera switch
-		
 	}
 
+	public void EnableAssistant()
+	{
+		Button btn = EnableAssistantButton.GetComponent<Button>();
+		VoiceManager assistant = Assistant.GetComponent<VoiceManager>();
+		// btn.onClick.AddListener(Assistant.StartListening);
+
+		btn.onClick.AddListener(assistant.OnSpeakStart);
+
+	}
+
+	public void DisableAssistant()
+	{
+		Button btn = DisableAssistantButton.GetComponent<Button>();
+		VoiceManager assistant = Assistant.GetComponent<VoiceManager>();
+		// btn.onClick.AddListener(Assistant.StopListening);
+		btn.onClick.AddListener(assistant.OnSpeakStop);
+	}
 
 }
