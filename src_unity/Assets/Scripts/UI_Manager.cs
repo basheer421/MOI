1255cf141ae9e46a77c4311c511b3158433c5741
Salam Sibai <salam.sibai@hotmail.com>
2024-05-12 09:45:55 -0400

    implemented sign in & sign in fade anim & NavBar

--- src_unity/Assets/Scripts/UI_Manager.cs
+++ src_unity/Assets/Scripts/UI_Manager.cs
@@ -1,30 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UI_Manager : MonoBehaviour
 {
-	// public Text uiText;
 	public List<user_info> info;
 	public int user;
 
+	[Header("Canvases")]
+	public	Canvas NavBarCanvas;
+	public	GameObject SignInPage;
+	public	GameObject FakeLoadScreen;
+	public	GameObject Home;
 	public void sign_in()
 	{
 		int rand = Random.Range(0, 2);
 		user = rand;
 		Debug.Log("the user selected is " + info[user]);
-		//switch 
+		StartCoroutine("SignInIEnum");
 	}
+
+	IEnumerator SignInIEnum()
+	{
+		SignInPage.transform.GetChild(2).gameObject.SetActive(false);
+		SignInPage.GetComponent<Animator>().SetBool("Fade", true);
+		yield return new WaitForSeconds(1.7f);
+		SignInPage.GetComponent<Canvas>().enabled = false;
+		FakeLoadScreen.GetComponent<Canvas>().enabled = true;
+		yield return new WaitForSeconds(3f);
+		FakeLoadScreen.GetComponent<Canvas>().enabled = false;
+		Home.GetComponent<Canvas>().enabled = true;
+		NavBarCanvas.enabled = true;
+	}
+
+
 	public void undeveloped_services()
 	{
 		Debug.Log("Coming Soon!");
 	}
 
-	public void vehicle_services()
+	public void	vehicle_services()
 	{
 		//camera switch
+		
 	}
-
 }
